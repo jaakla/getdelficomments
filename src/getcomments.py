655e7c974bb2126badcccc7826472fe9baf901b5
Jaak <jaak@nutiteq.com>
2017-10-13 23:16:44 +0300

    taavi rõiva fix

--- src/getcomments.py
+++ src/getcomments.py
@@ -13,7 +13,7 @@ import sys
 
 # siia artiklite algesed url-id, üks rea kohta:
 ariklid = '''
-http://publik.delfi.ee/news/inimesed/delfi-fotod-poliitikud-saabusid-sobraliku-eesti-kontserdile-peredega?id=72382353
+http://publik.delfi.ee/news/inimesed/video-blogi-ja-fotod-luisa-roivas-oma-abikaasa-umber-lahvatanud-ahistamisskandaalist-olen-hairitud-ja-taavis-pettunud?id=79814146&com=1&reg=0&no=0&s=1
 '''
 
 # http://www.delfi.ee/news/paevauudised/eesti/pealtnagija-eesti-pagulasvastased-vandenouteoreetikud-jalitavad-valismaalasi?id=73224487
@@ -27,8 +27,8 @@ def printComments(of, h, artikkel, reg, url):
     comments = tree.xpath('//div[@class="comment-content-inner"]')
     authors = tree.xpath('//div[@class="comment-author"]')
     commentDates = tree.xpath('//div[@class="comment-date"]/text()')
-    commentVotesUp = tree.xpath('//div[@class="comment-votes-up"]/a/text()')
-    commentVotesDown = tree.xpath('//div[@class="comment-votes-down"]/a/text()')
+    commentVotesUp = tree.xpath('//div[@class="comment-votes-up"]/a/span[@class="comment-votes-count"]/text()')
+    commentVotesDown = tree.xpath('//div[@class="comment-votes-down"]/a/span[@class="comment-votes-count"]/text()')
     commentsPagerNext = tree.xpath('//div[@class="comments-pager comments-pager-top"]/a[@class="comments-pager-arrow-last"]')
     
 # trüki üldtulemused, kontrolliks lihtsalt
@@ -44,15 +44,19 @@ def printComments(of, h, artikkel, reg, url):
     for i in range(len(commentDates)):
         # vote-d on kaks korda miskipärast, seega 
         ii = i * 2 + 1
+        
         # puhasta comment erinevast HTML läbust, reavahe jms
+        #comment = comments[i]
         comment = h.unescape(html.tostring(comments[i])).replace("</div>", "").replace("<div class=\"comment-content-inner\">", "").strip()
+        
         # reavahe asendus
         comment = comment.replace("<br>\n", u'⏎')
         # DELFI spam ära
         comment = comment.replace('<font class="delfiCoB">D</font><font class="delfiCoY">E</font><font class="delfiCoB">LFI</font>', 'DELFI')
         # puhasta autor HTML-ist
         author = h.unescape(html.tostring(authors[i])).replace("</div>", "").replace("<div class=\"comment-author\">", "").replace('<a href="javascript:void(1)" onclick="CommentList.showUserComments(this)">', "").replace("</a>", "").strip()
-        print >> of, artikkel, separator, reg, separator, i, separator, author.encode('utf-8'),separator, commentDates[i].strip(), separator, commentVotesUp[ii].strip(), separator, commentVotesDown[ii].strip(), separator , comment.encode('utf-8')
+        
+        print >> of, artikkel, separator, reg, separator, i, separator, author.encode('utf-8'),separator, commentDates[i].strip(), separator, commentVotesUp[i].strip(), separator, commentVotesDown[i].strip(), separator , comment.encode('utf-8')
 
     # kui on veel kommentaaride lehti, sisi võta pager-ist URL-id ja päri neid ka, kasutades sama funktsiooni (1-kordne rekursioon)
     for pager in commentsPagerNext: # seal on 1 tegelikult alati vaid, või 0 kui pole järgmist lehekülge
@@ -74,7 +78,7 @@ h = HTMLParser.HTMLParser()
 
 for artikkel in ariklid.split( ):
     
-    for reg in range(0, 2): # 0 - anon, 1 - registered user
+    for reg in range(2): # 0 - anon, 1 - registered user
         
         url = artikkel + "&com=1&reg=%s&no=0&s=1" % (reg)
         
